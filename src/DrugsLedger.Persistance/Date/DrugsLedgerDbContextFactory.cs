be4b10613cf07841cf1a81bde23bc7c1aa87549a
MichaelAns <mi035@mail.ru>
2023-03-01 11:35:45 +0300

    ConnectionString in json-file

--- src/DrugsLedger.Persistance/Date/DrugsLedgerDbContextFactory.cs
+++ src/DrugsLedger.Persistance/Date/DrugsLedgerDbContextFactory.cs
@@ -1,5 +1,6 @@
-﻿
+﻿using System.Text.Json;
 using Microsoft.EntityFrameworkCore.Design;
+using DrugsLedger.Persistance.Date.ConnectionString;
 
 namespace DrugsLedger.Persistance.Date
 {
@@ -8,8 +9,19 @@ namespace DrugsLedger.Persistance.Date
         public DrugsLedgerDbContext CreateDbContext(string[] args = null)
         {
             var options = new DbContextOptionsBuilder<DrugsLedgerDbContext>();
-            options.UseNpgsql("Host=localhost;Port=5432;Database=DrugsLedger;Username=postgres;Password=qwertyfsy");
+            //options.UseNpgsql("Host=localhost;Port=5432;Database=DrugsLedger;Username=postgres;Password=qwertyfsy");
+            string cs = GetConnectionStringFromFile();
+            options.UseNpgsql(cs);
             return new DrugsLedgerDbContext(options.Options);
+        }
+
+        private string GetConnectionStringFromFile()
+        {
+            string path = @"Resources\ConnectionString.json";
+            DrugsLedger.Persistance.Date.ConnectionString.
+                ConnectionString? connection = JsonSerializer.Deserialize<DrugsLedger.Persistance.Date.ConnectionString.
+                ConnectionString>(File.ReadAllText(path));
+            return $"Host={connection.Host};Port={connection.Port};Database={connection.Database};Username={connection.Username};Password={connection.Password}";
         }
     }
 }
